bd9db3885a84b4c4c2e7cbc86f61aba6efed4684
Freench <f.pernot@codeur.online>
2021-09-22 16:01:26 +0200

    Header, Footer, début Main

--- index.php
+++ index.php
@@ -5,10 +5,161 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projet data deezer </title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-    echo 'test';
-    ?>
+    <header>
+        <nav>
+            <div id="nav-left">
+                <div id="btn-menu-burger">
+                    <div id="menu-burger">
+                        <div class="long-line-burger"></div>
+                        <div class="long-line-burger"></div>
+                        <div class="short-line-burger"></div>
+                    </div>
+                    <!-- <img src="" alt="Burger Menu"> -->
+                </div>
+                <div class="btn-logo-appli">
+                    <div class="logo-appli">
+                        <div class="line-sound-1"></div>
+                        <div class="line-sound-2"></div>
+                        <div class="line-sound-3"></div>
+                        <div class="line-sound-4"></div>
+                        <div class="line-sound-5"></div>
+                        <div class="line-sound-6"></div>
+                        <div class="line-sound-7"></div>
+                    </div>
+                </div>
+                <div id="nav-title">
+                    <p class="blue-title">Music Band</p>
+                    <p class="blue-sub-title">La musique au bout des doigts</p>
+                </div>
+            </div>
+            <div id="nav-right">
+                <div id="div-research">
+                    <input type="text" value="Recherchez, écoutez...">
+                    <div class="blue-square">
+                        <img class="icon" src="icones/helmet.svg" alt="Icon helmet">
+                    </div>
+                </div>
+                <div id="identify">
+                    <img src="images/photo_alain.png" alt="photo of you">
+                    <div class="blue-title">Alain</div>
+                </div>
+            </div>
+        </nav>
+        <div id="header-main">
+            <div id="container-title">
+                <h1><span class="lightblue">Music</span> Band</h1>
+                <p>Votre musique sans pub, partout. Seulement 9,99 €/mois ensuite.<br> Soumis à conditions. Des podcasts exclusifs.<br> Des playlists sur-mesure. Faites des découvertes.<br> Zappez à volonté. Plus de 50 M de chansons.<br> Annulation à tout moment.
+                </p>
+                <div>
+                    <button class="blue-button">commencer l'écoute</button>
+                    <button class="blue-button">s'abonner</button>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div id="kind">
+            <h4>Genre</h4>
+            <div class="carousel">
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+            </div>
+        </div>
+        <div id="top-10">
+            <dic class="top-10-col">
+                <h4><span>Top 10 </span> des morceaux les plus écoutés</h4>
+                <div class="item-music">
+                    <p>01</p>
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-music">
+                    <div class="info-music">
+                        <p class="title-music">Bad Habbits</p>
+                        <p class="artist-music">Ed Sheeran</p>
+                    </div>
+                    <p class="duration">00:00</p>
+                    <div>white-line</div>
+                </div>
+            </dic>
+        </div>
+        <div class="artist-of-moment">
+
+        </div>
+        <div id="playlist">
+
+        </div>
+        <div id="podcast"></div>
+
+    </main>
+
+    <footer>
+        <div class="footer-col">
+            <div class="flex-row">
+                <div class="btn-logo-appli">
+                    <div class="logo-appli">
+                        <div class="line-sound-1"></div>
+                        <div class="line-sound-2"></div>
+                        <div class="line-sound-3"></div>
+                        <div class="line-sound-4"></div>
+                        <div class="line-sound-5"></div>
+                        <div class="line-sound-6"></div>
+                        <div class="line-sound-7"></div>
+                    </div>
+                </div>
+                <div class="nav-title">
+                    <p class="blue-title">Music Band</p>
+                    <p class="blue-sub-title">La musique au bout des doigts</p>
+                </div>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit consequuntur eligendi iusto libero delectus nisi, ratione neque, vel expedita illum tenetur atque porro in sunt autem vitae, excepturi eveniet obcaecati!
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, magni non quo incidunt commodi nostrum, eum architecto eligendi ratione nesciunt excepturi, illum labore impedit saepe suscipit voluptas aut delectus facere.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum facilis repellat odit, illum dignissimos ad maxime a, beatae atque nesciunt, at assumenda amet! Libero, vitae id incidunt delectus veritatis nam!</p>
+        </div>
+        <div class="footer-col">
+            <h4>Inscrivez-vous à la Newsletter</h4>
+            <p>Abonnez-vous à notre newsletter et recevez les dernières mises à jour et offres.</p>
+            <input type="text" value="Entrez votre nom">
+            <input type="text" value="Entrez votre email">
+            <button class="blue-button">inscrivez-vous</button>
+        </div>
+        <div class="footer-col">
+            <h4>Nous contacter</h4>
+            <p>Vous souhaitez nous contacter.<br>Plusieurs solutions s'offrent à vous</p>
+            <div class="flex-row-none">
+                <div class="blue-square-footer">
+                    <img src="icones/phone-call-outline.svg" alt="icon phone">
+                </div>
+                <div>
+                    <p>Par téléphone au :</p>
+                    <p class="clickable-line">03 89 87 85 86</p>
+                </div>
+            </div>
+            <div class="flex-row-none margin-bot">
+                <div class="blue-square-footer">
+                    <img src="icones/mail-open.svg" alt="icon letter">
+                </div>
+                <div>
+                    <p>Par email au :</p>
+                    <p class="clickable-line">contact[@]musicband.com</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div id="lecteur">
+
+    </div>
+
+    <!-- <script src="scrypt.js"></script> -->
 </body>
 </html>
