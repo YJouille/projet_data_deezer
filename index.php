d8fc45ecb4b8fa14e98419187ba66c651470ed7c
Freench <f.pernot@codeur.online>
2021-09-23 16:54:52 +0200

    Début carou et artist en vogue

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                 <div id="div-research">
                     <input type="text" value="Recherchez, écoutez...">
                     <div class="blue-square">
-                        <img class="icon" src="icones/helmet.svg" alt="Icon helmet">
+                        <img class="icon" src="icons/helmet.svg" alt="Icon helmet">
                     </div>
                 </div>
                 <div id="identify">
@@ -64,6 +64,26 @@
         <div id="kind">
             <h4>Genre</h4>
             <div class="carousel">
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
+                <div class="item-kind">
+                    <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
+                    <p class="title-kind">PoP</p>
+                </div>
                 <div class="item-kind">
                     <img src="https://image.shutterstock.com/image-illustration/musician-guitar-rock-guitarist-player-260nw-1112534567.jpg" alt="" class="img-kind">
                     <p class="title-kind">PoP</p>
@@ -168,8 +188,13 @@
                 <!-- FINTEST -->
             </div>
         </div>
-        <div class="artist-of-moment">
-
+        <div id="artist-of-moment">
+            <div class="player-artist">
+                <div class="blue-circle">
+                    <img class="icon-play" src="icons/play.svg" alt="icon play-pause">
+                </div>
+                <h2>Découvrez l'artiste du moment <br> NINHO</h2>
+            </div>
         </div>
         <div id="playlist">
 
@@ -213,7 +238,7 @@
             <p>Vous souhaitez nous contacter.<br>Plusieurs solutions s'offrent à vous</p>
             <div class="flex-row-none">
                 <div class="blue-square-footer">
-                    <img src="icones/phone-call-outline.svg" alt="icon phone">
+                    <img src="icons/phone-call-outline.svg" alt="icon phone">
                 </div>
                 <div>
                     <p>Par téléphone au :</p>
@@ -222,7 +247,7 @@
             </div>
             <div class="flex-row-none margin-bot">
                 <div class="blue-square-footer">
-                    <img src="icones/mail-open.svg" alt="icon letter">
+                    <img src="icons/mail-open.svg" alt="icon letter">
                 </div>
                 <div>
                     <p>Par email au :</p>
@@ -235,6 +260,6 @@
 
     </div>
 
-    <!-- <script src="scrypt.js"></script> -->
+    <script src="script.js"></script>
 </body>
 </html>
